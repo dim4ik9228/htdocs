171ee14fd504b351f3bddd3502fb76f78e213fcb
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2023-05-07 19:27:27 +0300

    lab6

--- blog/app/Repositories/BlogCategoryRepository.php
+++ blog/app/Repositories/BlogCategoryRepository.php
@@ -30,7 +30,47 @@ class BlogCategoryRepository extends CoreRepository
      */
     public function getForComboBox()
     {
-        return $this->startConditions()->all();
+        // return $this->startConditions()->all;
+        $columns = implode(', ', [
+            'id',
+            'CONCAT (id, ". ", title) AS id_title',  //додаємо поле id_title
+        ]);
+
+        //$result = $this->startConditions()->all();
+        /*$result = $this                           //1 варіант
+            ->startConditions()
+            ->select('blog_categories.*',
+                \DB::raw('CONCAT (id, ". ", title) AS id_title'))
+            ->toBase()                              //не робити колекцію(масив) BlogCategory, отримати дані у вигляді класу
+            ->get();*/
+
+        $result = $this                           //2 варіант
+        ->startConditions()
+            ->selectRaw($columns)
+            ->toBase()
+            ->get();
+
+        //dd($result);
+
+        return $result;
+    }
+    /**
+     * Отримати категорію для виводу пагінатором
+     *
+     * @param int|null $perPage
+     *
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function getAllWithPaginate($perPage = null)
+    {
+        $columns = ['id', 'title', 'parent_id'];
+
+        $result = $this
+            ->startConditions()
+            ->select($columns)
+            ->paginate($perPage); //можна $columns додати сюди
+
+        return $result;
     }
 
 }
